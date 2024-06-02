d575e8ab7dd6afb6f68ebbe71b7bb677ca061409
Chafah Sani <125821852+CHAFAH@users.noreply.github.com>
2024-06-02 14:37:05 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -53,14 +53,21 @@ resource "aws_security_group" "allow_ssh_http" {
   ingress {
     from_port   = 22
     to_port     = 22
-    protocol    = "tcp"
+    protocol    = "ssh"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "https"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 
   ingress {
     from_port   = 80
     to_port     = 80
-    protocol    = "tcp"
+    protocol    = "http"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
