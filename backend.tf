cd7d549055790a3da5a3133b7af88b6e46103de0
Chafah Sani <125821852+CHAFAH@users.noreply.github.com>
2024-06-02 11:00:48 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-bucket"
+    bucket         = "terraform-state-bucket-devops-hilltop"
     key            = "terraform/state"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
