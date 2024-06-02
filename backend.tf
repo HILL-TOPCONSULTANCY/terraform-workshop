aa0b8774aaf819deaed0285bbbfb7628c7b5962e
Chafah Sani <125821852+CHAFAH@users.noreply.github.com>
2024-06-02 13:33:08 +0200

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-bucket-devops-hilltop"
-    key            = "terraform/state"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
