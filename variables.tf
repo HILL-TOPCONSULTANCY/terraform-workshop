98cef451cd5a7c75e19782acad938a1ef4ce0205
Chafah Sani <125821852+CHAFAH@users.noreply.github.com>
2024-06-02 13:34:23 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,16 @@
-variable "vpc_cidr" {
-  description = "The CIDR block for the VPC"
-  default     = "10.0.0.0/16"
+variable "ami" {
+  description = "AMI ID for the instance"
+  type        = string
+  default     = "ami-0d191299f2822b1fa" # Amazon Linux 2 AMI
 }
 
-variable "public_subnets" {
-  description = "List of public subnet CIDRs"
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+variable "instance_type" {
+  description = "Instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
 }
