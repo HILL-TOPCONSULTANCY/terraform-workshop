dead61ed96fa454c974fa0c128170f285e31f332
Chafah Sani <125821852+CHAFAH@users.noreply.github.com>
2024-06-02 10:09:31 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,19 @@
-variable "region" {
-  description = "AWS region"
-  default     = "us-east-1"
-  type        = string
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDRs"
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
-  default     = "t2.medium"
-  type        = string
+  default     = "t2.micro"
 }
 
 variable "ami" {
-  description = "AMI ID"
-  default     = "ami-0d191299f2822b1fa"
-  type        = string
+  description = "AMI ID for the instances"
+  default     = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
 }
